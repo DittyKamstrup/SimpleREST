e274a77a0d27b683b5946becc89e6d07f45d2481
DittyKamstrup <ditt4030@edu.easj.dk>
2018-01-04 13:43:59 +0100

    Class Chips added, IService1 & Service1 updated

---
+++ SimpleREST/Chips.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleREST
+{
+    public class Chips
+    {
+        
+        //PROPERTIES
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        //DEFAULT CONSTRUCTOR
+        public Chips()
+        {}
+
+        //PARAMETERISED CONSTRUCTOR
+        public Chips(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        //TOSTRING
+        public override string ToString()
+        {
+            return "ID: " + Id + ", Navn: " + Name;
+        }
+
+    }
+}
--- SimpleREST/Service1.svc.cs
+++ SimpleREST/Service1.svc.cs
@@ -12,22 +12,17 @@ namespace SimpleREST
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
     {
-        public string GetData(int value)
-        {
-            return string.Format("You entered: {0}", value);
-        }
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        private static List<Chips> chipsList = new List<Chips>
         {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
+            new Chips(1, "Pringles"),
+            new Chips(2, "Lays"),
+            new Chips {Id = 3, Name = "Taffel"}
+        };
+
+        public List<Chips> GetAllChips()
+        {
+            return chipsList;
         }
     }
 }
